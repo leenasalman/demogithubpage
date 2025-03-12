4493feebbc6246917a332ad0fc98aab730bcc50b
leenasalman <57545008+leenasalman@users.noreply.github.com>
2025-03-12 19:43:43 +0400

    Delete fetch-reviews.php

--- fetch-reviews.php
+++
@@ -1,9 +0,0 @@
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-
-$apiKey = "AIzaSyCiGJQRJQ_IyycIgeXIqVVVspoj_7i55YE";
-$placeId = "ChIJXdA7Y5FpXz4Rl0MVSjKjx9U";
-$url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&fields=reviews&key=$apiKey";
-
-$response = file_get_contents($url);
-echo $response;
